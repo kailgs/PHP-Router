38bc1257f152238bbc9a060eb3b1ff25aa339497
kailgs <kai.herbst@online.de>
2022-07-22 00:05:56 +0200

    Added some example routes

--- index.php
+++ index.php
@@ -4,16 +4,22 @@ require('router.php');
 $router = new Router("/dpsg");
 
 $router->route('/', function() {
-
+    echo "Das ist die Startseite!";
 });
 
-$router->route('/gruppen', function() {
+$router->route('/gruppen/{id}/{pepeLaugh}', function($id, $pepe) {
+    echo "Das ist die Gruppenseite!<br>";
+    echo "Mit der Nummer: ".$id." UND DAS IST: ". $pepe;
+});
 
+$router->route('/gruppen/{id}/jamoins', function($id) {
+    echo "Das ist die ANDERE Gruppenseite!<br>";
+    echo "Mit der NUMMEr: ".$id;
 });
 
 $router->route('/kontakt', function() {
-
+    echo "Das ist die Kontaktseite!";
 });
 
-$router->printRoutes();
+$router->run();
 ?>
